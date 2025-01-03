e99c4b65c6ce0742e069691233920644d81aa8ec
Vlado <vprokhorov.work@gmail.com>
2025-01-03 17:18:22 +0200

    fix Exclude import for user schema

--- src/auth/schemas/user.schema.ts
+++ src/auth/schemas/user.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { Exclude } from "class-transformer";
 
 @Schema()
 export class User extends Document {
