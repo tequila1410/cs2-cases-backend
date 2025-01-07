fc7b0128462f35ba537122363e63051764f7d95f
Vlado <vprokhorov.work@gmail.com>
2025-01-07 22:46:57 +0200

    *

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   //   prefix: '/uploads/',
   // });
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
